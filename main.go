e65c31b7d2e67557d88e88918619f5bf8d638146
bannzai <kingkong999yhirose@gmail.com>
2019-10-27 10:55:09 +0900

    :sparkles: Passing terminal window size to window

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/bannzai/itree/pkg/parser"
 	"github.com/bannzai/itree/pkg/ui"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	if err := tview.NewApplication().SetRoot(ui.TreeUI(), true).Run(); err != nil {
+	size, err := parser.ParseSize()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tview.NewApplication().SetRoot(ui.NewWindow(size.Width, size.Height), true).Run(); err != nil {
 		panic(err)
 	}
 }
