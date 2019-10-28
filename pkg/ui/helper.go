226335a1fc5071a2f3057e6be2002c73de3fc055
bannzai <kingkong999yhirose@gmail.com>
2019-10-28 23:11:13 +0900

    :sparkels: Fix helpers functions

--- pkg/ui/helper.go
+++ pkg/ui/helper.go
@@ -1,8 +1,12 @@
 package ui
 
 import (
+	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/rivo/tview"
 )
 
 func absolutePath(node nodeReference) string {
@@ -17,4 +21,32 @@ func absolutePath(node nodeReference) string {
 func moveFile(from, to string) error {
 	mvCmd := exec.Command("mv", from, to)
 	return mvCmd.Run()
+}
+
+func createFile(path string) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return errors.Wrapf(err, "New file error. path for  %s", path)
+	}
+	defer file.Close()
+	return nil
+}
+
+func createTreeNode(fileName string, isDir bool, parent *tview.TreeNode) *tview.TreeNode {
+	var parentPath string
+	parentReference, ok := parent.GetReference().(*nodeReference)
+	if ok {
+		parentPath = parentReference.path
+	} else {
+		parentPath = "./"
+	}
+	return tview.NewTreeNode(fileName).
+		SetReference(
+			newNodeReference(
+				filepath.Join(parentPath, fileName),
+				isDir,
+				parent,
+			),
+		).
+		SetSelectable(true)
 }
