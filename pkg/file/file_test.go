1268cd30ed810ab609f265023fda1f87f8cf60eb
bannzai <kingkong999yhirose@gmail.com>
2019-10-29 19:53:37 +0900

    :shell: gotests -w -all file.go

---
+++ pkg/file/file_test.go
@@ -0,0 +1,64 @@
+package file
+
+import "testing"
+
+func TestFileExists(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FileExists(tt.args.filename); got != tt.want {
+				t.Errorf("FileExists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMoveFile(t *testing.T) {
+	type args struct {
+		from string
+		to   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := MoveFile(tt.args.from, tt.args.to); (err != nil) != tt.wantErr {
+				t.Errorf("MoveFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestMakeFile(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := MakeFile(tt.args.path); (err != nil) != tt.wantErr {
+				t.Errorf("MakeFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
