2c07684722a01703c861f8a57079d5ba97293d07
bannzai <kingkong999yhirose@gmail.com>
2019-10-30 22:38:05 +0900

    :sparkles: Add help command

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/bannzai/itree/pkg/parser"
 	"github.com/bannzai/itree/pkg/ui"
@@ -9,11 +10,26 @@ import (
 )
 
 var (
-	path = flag.String("path", "./", "Root path. Default is ./")
+	path = flag.String("path", "./", "specified start path. default is ./")
+	help = flag.Bool("help", false, "displayed help message for itree")
 )
 
 func main() {
 	flag.Parse()
+
+	if *help {
+		fmt.Printf(`itree displayed file system tree and command interactively about file system.
+
+Usage:
+	itree [options]
+
+The options are:
+	--path=$PATH specified start path. default is ./
+	--help       displayed help message for itree
+`,
+		)
+		return
+	}
 
 	size, err := parser.ParseSize()
 	if err != nil {
